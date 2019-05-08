e52ad562d6205f7bc5b341d5917d7e15431560dc
Machiel Cligge <machielc@hotmail.com>
2019-05-08 10:43:26 +0200

    now with bars

--- Homework/Week_4/toJson.py
+++ Homework/Week_4/toJson.py
@@ -5,5 +5,6 @@ df = pd.read_csv("input.csv", decimal=",")
 df = df.loc[df['TIME'] == 2017]
 df = df.loc[df['SUBJECT'] == "DTP"]
 df1 = df[['LOCATION','VALUE']]
+
 # Transform to Json
 df1.to_json('output.json', orient='records')
